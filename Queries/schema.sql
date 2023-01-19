87877537a4b6fbab4274e54a71897a1e25f02aee
jaime-mclean <akajaime@yahoo.com>
2023-01-18 17:30:58 -0700

    three files

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -64,21 +64,3 @@ CREATE TABLE titles (
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
-SELECT * FROM departments;
-
-SELECT first_name, last_name
-FROM employees
-WHERE (birthdate BETWEEN '1952-01-01' AND '1955-12-31') AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
-SELECT COUNT(first_name)
-FROM employees
-WHERE (birthdate BETWEEN '1952-01-01' AND '1955-12-31') 
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
-SELECT first_name, last_name
-INTO retirement_info
-FROM employees
-WHERE (birthdate BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
-SELECT * FROM retirement_info;
